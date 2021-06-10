78d69e4065301f9ea227f9b5d075e4674c700e4e
Cesar Morales <cmoralesl15@miumg.edu.gt>
2021-06-10 00:18:21 -0600

    Cesar Morales - Inicializacion del proyecto 2

--- BaseDatos.Backup/Properties/AssemblyInfo.cs
+++ BaseDatos.Backup/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("9692c381-c69d-4df9-814e-5e62ad02c846")]
+[assembly: Guid("f5899e76-7699-42ad-8beb-f617ee395d1e")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
