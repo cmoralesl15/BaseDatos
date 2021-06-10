16224994c4e48845687d8d951fa2d8f88afd0d2e
Cesar Morales <cmoralesl15@miumg.edu.gt>
2021-06-10 01:21:41 -0600

    Cesar Morales - Backup programado.

--- BaseDatos.Backup/Program.cs
+++ BaseDatos.Backup/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,22 @@ namespace BaseDatos.Backup
     {
         static void Main(string[] args)
         {
+            string rutaArchivoOrigen;
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(ConfigurationManager.AppSettings["storedProcedure"].ToString(), connection))
+                {
+                    SqlParameter sqlParameter = new SqlParameter("@Valor", SqlDbType.VarChar, -1);
+                    sqlParameter.Direction = ParameterDirection.Output;
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        rutaArchivoOrigen = command.Parameters["@Valor"].Value.ToString();
+                    }
+                }
+            }
+            string[] array = rutaArchivoOrigen.Split('\\');
+            string rutaArchivoDestino = "C:\\Users\\Administrator\\OneDrive - Universidad Mariano Gálvez\\Backup" + array[array.Length - 2] + array[array.Length - 1];
+            File.Copy(rutaArchivoOrigen,"");
         }
     }
 }
